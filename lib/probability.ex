f9faa114d63cc08fd497f260962007d2dc4dfcf0
Kristian Larsson <kristian@tutamail.com>
2018-11-05 12:47:40 -0800

    DRYed up the display

--- lib/probability.ex
+++ lib/probability.ex
@@ -18,17 +18,7 @@ defmodule Probability do
 
   defp get_percent(rm, correct, iterations) do
     count = Map.get(rm, correct)
-    count * 100.0 / iterations |> Float.round(5)
-  end
-
-  defp display_result(rm, i) do
-    IO.puts "0 correct: #{Map.get(rm, 0)}, #{get_percent(rm, 0, i)}%"
-    IO.puts "1 correct: #{Map.get(rm, 1)}, #{get_percent(rm, 1, i)}%"
-    IO.puts "2 correct: #{Map.get(rm, 2)}, #{get_percent(rm, 2, i)}%"
-    IO.puts "3 correct: #{Map.get(rm, 3)}, #{get_percent(rm, 3, i)}%"
-    IO.puts "4 correct: #{Map.get(rm, 4)}, #{get_percent(rm, 4, i)}%"
-    IO.puts "5 correct: #{Map.get(rm, 5)}, #{get_percent(rm, 5, i)}%"
-    IO.puts "6 correct: #{Map.get(rm, 6)}, #{get_percent(rm, 6, i)}%"
+    count * 100.0 / iterations |> Float.round(3)
   end
 
   def start(iterations \\ 10000000) do
@@ -37,7 +27,9 @@ defmodule Probability do
     result_map = Enum.reduce(1..iterations, %{0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0}, fn(_i, acc) ->
       Map.update!(acc, run_six(), &(&1 + 1))
     end)
-    display_result(result_map, iterations)
+    Enum.each(0..6, fn i ->
+      IO.puts "#{i} correct: #{Map.get(result_map, i)}, #{get_percent(result_map, i, iterations)}%"
+    end)
     DateTime.diff(DateTime.utc_now(), start_time, :millisecond)
   end
 end
